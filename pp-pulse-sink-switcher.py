b9724e2d145b92d7a849dd411fd94c3e02299194
Piotr Pokojowczyk <piotrpokojowczyk@gmail.com>
2018-06-13 19:48:47 +0200

    added default value for radiobox group

--- pp-pulse-sink-switcher.py
+++ pp-pulse-sink-switcher.py
@@ -8,11 +8,12 @@ class Application(tk.Frame):
     class Sink(object):
         display = ""
         id = ""
+        default = bool
 
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.setSinks()
-        self.master.config(borderwidth = 10);
+        self.master.config(borderwidth = 10)
         self.grid(row = 0, column = 0)
         self.addSinksFrame()
         self.renderSinks()
@@ -34,22 +35,31 @@ class Application(tk.Frame):
 
     def setSinks(self):
         self.sinks = []
-        output = subprocess.check_output(['pactl', 'list', 'sinks']);
-        result = re.split("Sink #.", output);
+        output = subprocess.check_output(['pactl', 'list', 'sinks'])
+        result = re.split("Sink #.", output)
+
+        output = subprocess.check_output(['pactl', 'info'])
+        defaultSink = re.findall(".*Default Sink: (.*)", output)[0]
+
         result.pop(0)
         for str in result:
             name = re.findall(".*Name: (.*)", str)[0]
             description = re.findall(".*Description: (.*)", str)[0]
-            sink1 = self.Sink()
-            sink1.name = name
-            sink1.description = description
-            self.sinks.append(sink1)
+            sink = self.Sink()
+            sink.name = name
+            sink.description = description
+            sink.default = defaultSink == name and True or False
+            self.sinks.append(sink)
 
     def renderSinks(self):
+        var = tk.IntVar()
+        count = 1
         for sink in self.sinks:
-            tk.Radiobutton(self.sinksFrame, text=sink.description, variable='', value=sink.name, command=lambda sink=sink: self.changeSink(sink)).grid(sticky='w')
+            tk.Radiobutton(self.sinksFrame, text=sink.description + ' (' + sink.name + ')', variable = var, value=count, command=lambda sink=sink: self.changeSink(sink, var)).grid(sticky='w')
+            sink.default and var.set(count)
+            count = count+1
 
-    def changeSink(self, sink):
+    def changeSink(self, sink, var):
         subprocess.check_output(['pacmd', 'set-default-sink', sink.name])
         output = subprocess.check_output(['pacmd', 'list-sink-inputs'])
         inputsIndexes = re.findall(".*index: (.*)", output)
